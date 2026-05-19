1cce917d24f883ba0fdc58a5b492c1c09bb15a18
Solarfresh33 <maxime210476@gmail.com>
2026-05-19 11:50:59 +0200

    add license

--- camille/cli.py
+++ camille/cli.py
@@ -40,7 +40,7 @@ BANNER = r"""
    ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚══════╝╚══════╝╚══════╝
 
   Cyber Audit & Monitoring Intelligence for Local and Large Environments
-  For authorized use only — v2.1.0
+  For authorized use only — v1.0.0
 """
 
 
